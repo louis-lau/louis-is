7d01ee5d1711201864986df8ebdc7e8b8128f07c
Louis Laureys <louis@laureys.me>
2020-05-06 19:59:37 +0200

    Only play music on interaction

--- html/sexy/javascript.js
+++ html/sexy/javascript.js
@@ -1,10 +1,15 @@
 const soundcloudIframe = document.getElementById('soundcloud')
+const body = document.body
 
-const reloadIframe = ()=> {
+const reloadIframe = () => {
   setTimeout(() => {
     soundcloudIframe.src += ''
     reloadIframe()
   }, 35000);
 }
 
-reloadIframe()
+body.addEventListener('click', event => {
+  soundcloudIframe.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/259055823&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+  
+  reloadIframe()
+}, {once: true});
